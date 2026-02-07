dd1527a8e264b064aa01615d091919df76d3029e
Durvesh Bari <durveshbari83@gmail.com>
2026-02-07 15:45:12 +0530

    commit 3: database connection built

--- backend/database.js
+++ backend/database.js
@@ -1,3 +1,22 @@
 const mysql = require("mysql2");
 
-const db = mysql.create
+const db = mysql.create(
+    {
+        host :"localhost",
+        user:"root",
+        password:"Dasb4804",
+        database:"LOGS",
+    });
+
+
+db.connect(err => 
+    {
+        if(err)
+        {
+            console.error("Database connection terminated",err);
+            return;
+        }
+        console.log("Database connection successful");
+    });
+
+    module.exports = db;
