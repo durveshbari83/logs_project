3d4bb5ec05047c7517b7715e5909701750278b25
Mehul Tiwari <mehultiwari2006@gmail.com>
2026-02-07 19:05:02 +0530

    ..

--- backend/database.js
+++ backend/database.js
@@ -2,21 +2,19 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection(
     {
-        host :"localhost",
-        user:"root",
-        password:"DAsb4804",
-        database:"LOGS",
+        host: "localhost",
+        user: "root",
+        password: "mehul123",
+        database: "LOGS",
     });
 
 
-db.connect(err => 
-    {
-        if(err)
-        {
-            console.error("Database connection terminated",err);
-            return;
-        }
-        console.log("Database connection successful");
-    });
+db.connect(err => {
+    if (err) {
+        console.error("Database connection terminated", err);
+        return;
+    }
+    console.log("Database connection successful");
+});
 
-    module.exports = db;
+module.exports = db;
---
+++ front_end/website/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from './components/home/home-page/home-page.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HomePageComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'website';
+}
