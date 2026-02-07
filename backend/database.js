010ca63bbaa4029809a090d60fa3c2d16cd58ffc
Mehul Tiwari <mehultiwari2006@gmail.com>
2026-02-07 23:16:53 +0530

    commit4:datatable

--- backend/database.js
+++ backend/database.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection(
     {
         host: "localhost",
         user: "root",
-        password: "DAsb4804",
+        password: "mehul123",
         database: "LOGS",
     });
 
